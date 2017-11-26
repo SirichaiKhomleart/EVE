99f2aa5e5dfd2cdcdab1268f6d8e514f51f30bd2
nuttapol <nuttapol.sb@hotmail.com>
2017-11-26 19:59:45 +0700

    add insert pic button

--- createevent.php
+++ createevent.php
@@ -22,6 +22,10 @@
 	<link rel="stylesheet" type="text/css" href="css/prettyPhoto.css" />
 	<link rel="stylesheet" type="text/css" href="css/unslider.css" />
 	<link rel="stylesheet" type="text/css" href="css/template.css" />
+	<link rel="stylesheet" type="text/css" href="css/component.css" />
+
+
+
 	
 </head>
 
@@ -34,7 +38,6 @@
     js.src = "//connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v2.10";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
-	
 
 	<!-- Header
     ================================================== -->
@@ -176,8 +179,8 @@
 									<span class="quotet">Event Poster :</span>
 								</div>
 								<div class="col-md-9 pull-left">
-									<input class="input2" type="text" name="eventtime">
-									<a href=".nino-testimonial" class="nino-btnc pull-right">Browse</a>
+									<input type="file" name="file-2[]" id="file-2" class="inputfile inputfile-2" data-multiple-caption="{count} files selected" multiple />
+									<label for="file-2"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"/></svg> <span>Choose a file&hellip;</span></label>
 								</div>
 								<div style="clear:both;"></div>
 							</div>
@@ -294,6 +297,6 @@
 	<script type="text/javascript" src="js/jquery.mCustomScrollbar.concat.min.js"></script>
 	<script type="text/javascript" src="js/unslider-min.js"></script>
 	<script type="text/javascript" src="js/template.js"></script>
-		
+	<script src="js/custom-file-input.js"></script>
 </body>
 </html>
